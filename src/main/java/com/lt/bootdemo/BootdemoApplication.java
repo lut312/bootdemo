9e7453380917b77e2d6a93b0772da530566aa9db
lut <1939713029@qq.com>
2018-07-27 13:37:02 +0800

    thymeleaf

--- src/main/java/com/lt/bootdemo/BootdemoApplication.java
+++ src/main/java/com/lt/bootdemo/BootdemoApplication.java
@@ -1,16 +1,28 @@
 package com.lt.bootdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
 public class BootdemoApplication {
+
+    @Value("${name}")
+    private String name;
+    @Value("${want}")
+    private String want;
+    @Autowired
+    ConfigBean configBean;
+
     @RequestMapping("/")
     public String index(){
-        return "hello spring boot";
+        configBean.setName("lut");
+        return name + " " + want + " " + configBean.getName();
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/com/lt/bootdemo/ConfigBean.java
@@ -0,0 +1,25 @@
+package com.lt.bootdemo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "com.lt")
+public class ConfigBean {
+    private String name;
+    private String want;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWant() {
+        return want;
+    }
+
+    public void setWant(String want) {
+        this.want = want;
+    }
+}
